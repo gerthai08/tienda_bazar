704304cfc801441ef15668994a4e22747c4c5c80
gerthai08 <germanrgutierre@gmail.com>
2025-02-26 18:03:35 -0300

    26-02-25

---
+++ src/main/java/com/tienda_bazar/TiendaBazarApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_bazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda_bazar/TiendaBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_bazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaBazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
